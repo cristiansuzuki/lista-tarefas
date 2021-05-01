049e6260bfc8e888e77071f389465fbbf4b4b62c
FranciscoGomes20 <henriquecgomes10@gmail.com>
2021-05-01 15:35:55 -0500

    Add uma Modal de form

--- listaApp/models.py
+++ listaApp/models.py
@@ -1,11 +1,7 @@
-# Create your models here.
-
 from django.db import models
 from django.utils import timezone
 
 # Tipo (ex: Trabalho, Estudo, etc...)
-
-
 class Categoria(models.Model):
     tipo = models.CharField(max_length=100)
 
@@ -17,15 +13,11 @@ class Categoria(models.Model):
         return self.tipo  # retorna o nome da categoria quando chamada
 
 # Lista com suas respectivas caracteristicas, pegadno categoria como FOREIGN KEY
-
-
 class Lista(models.Model):
     titulo = models.CharField(max_length=300)
     conteudo = models.TextField(blank=True)
-    data_criacao = models.DateField(
-        default=timezone.now().strftime("%Y-%m-%d"))
-    data_termino = models.DateField(
-        default=timezone.now().strftime("%Y-%m-%d"))
+    data_criacao = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
+    data_termino = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
 
     def __str__(self):
